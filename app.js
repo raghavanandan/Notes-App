fb5726aafcacef15c2909dc5c99930ff87f14302
Raghav <raghavmw@gmail.com>
2018-01-16 00:58:38 -0800

    Listing notes

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,27 +6,31 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
-console.log('Command :', command);
-console.log('Yargs', argv);
 
 if(command === 'add'){
   var note = notes.addNote(argv.title, argv.body);
   if (note) {
     console.log('Note created');
-    console.log('---');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    notes.logNote(note);
   } else {
     console.log('Note title already exists');
   }
 } else if (command === 'list') {
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('Note fetched');
+    notes.logNote(note);
+  } else {
+    console.log('Note not found');
+  }
 } else if (command == 'remove') {
   var noteRemoved = notes.removeNote(argv.title);
   var msg = noteRemoved ? 'Note was removed' : 'No note removed';
   console.log(msg);
 } else {
-  console.log('Command note recognized');
+  console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -31,11 +29,13 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 
 var getNote = (title) => {
-  console.log('Getting note', title);
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
 };
 
 var removeNote = (title) => {
@@ -45,9 +45,17 @@ var removeNote = (title) => {
   return notes.length !== filteredNotes.length
 };
 
+var logNote = (note) => {
+  debugger;
+  console.log('---');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+};
+
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 };
