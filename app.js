6a9b45b69077941c0173e5c4fa013ae5b003423b
Raghav <raghavmw@gmail.com>
2018-01-15 22:09:40 -0800

    Add and Remove Notes

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command :', command);
+console.log('Yargs', argv);
+
+if(command === 'add'){
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('Note created');
+    console.log('---');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  } else {
+    console.log('Note title already exists');
+  }
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
+} else if (command == 'remove') {
+  var noteRemoved = notes.removeNote(argv.title);
+  var msg = noteRemoved ? 'Note was removed' : 'No note removed';
+  console.log(msg);
+} else {
+  console.log('Command note recognized');
+}
